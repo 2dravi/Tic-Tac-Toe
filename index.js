186d615a659b4c15eadf3d3757adebf27a87c13e
Tudi Ravi Shanker <tudi.shanker@happay.in>
2019-01-22 20:43:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,208 @@
+/**
+* This program is a boilerplate code for the standard tic tac toe game
+* Here the “box” represents one placeholder for either a “X” or a “0”
+* We have a 2D array to represent the arrangement of X or O is a grid
+* 0 -> empty box
+* 1 -> box with X
+* 2 -> box with O
+*
+* Below are the tasks which needs to be completed:
+* Imagine you are playing with the computer so every alternate move should be done by the computer
+* X -> player
+* O -> Computer
+*
+* Winner needs to be decided and has to be flashed
+*
+* Extra points will be given for approaching the problem more creatively
+* 
+*/
+
+
+const USER_WINNING_MESSAGE = 'Yayy!!! You have won.';
+const USER_LOSE_MESSAGE = "Ooops!! You didnt win. Please try again";
+const GAME_OVER_MESSAGE = "Game Over!!! Please try again";
+let grid = [];
+const GRID_LENGTH = 3;
+let turn = 'X';
+function initializeGrid() {
+    grid = [];
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
+        const tempArray = [];
+        for (let rowidx = 0; rowidx < GRID_LENGTH; rowidx++) {
+            tempArray.push(0);
+        }
+        grid.push(tempArray);
+    }
+}
+
+function getRowBoxes(colIdx) {
+    let rowDivs = '';
+
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        let additionalClass = 'darkBackground';
+        let content = '';
+        const sum = colIdx + rowIdx;
+        if (sum % 2 === 0) {
+            additionalClass = 'lightBackground'
+        }
+        const gridValue = grid[colIdx][rowIdx];
+        if (gridValue === 1) {
+            content = '<span class="cross">X</span>';
+        }
+        else if (gridValue === 2) {
+            content = '<span class="cross">O</span>';
+        }
+        rowDivs = rowDivs + '<div colIdx="' + colIdx + '" rowIdx="' + rowIdx + '" class="box ' +
+            additionalClass + '">' + content + '</div>';
+    }
+    return rowDivs;
+}
+
+function getColumns() {
+    let columnDivs = '';
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
+        let coldiv = getRowBoxes(colIdx);
+        coldiv = '<div class="rowStyle">' + coldiv + '</div>';
+        columnDivs = columnDivs + coldiv;
+    }
+    return columnDivs;
+}
+
+function renderMainGrid() {
+    const parent = document.getElementById("grid");
+    const columnDivs = getColumns();
+    parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+}
+
+function onBoxClick() {
+    var rowIdx = this.getAttribute("rowIdx");
+    var colIdx = this.getAttribute("colIdx");
+    let newValue = 1;
+    if(grid[colIdx][rowIdx]===0){
+    grid[colIdx][rowIdx] = newValue;
+    renderMainGrid();
+    let gameOver =  checkWinner();
+    if(!gameOver){
+    renderSystemMove();
+    renderMainGrid();
+    checkWinner();
+    }
+    addClickHandlers();}
+}
+
+function isGridFull() {
+    let isGridFull = true;
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
+            if (grid[rowIdx][colIdx] === 0) {
+                isGridFull = false;
+                break;
+            }
+        }
+    }
+
+    return isGridFull;
+}
+
+function checkColumn(symbol, columnId) {
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        if (grid[rowIdx][columnId] !== symbol) {
+         return false
+        }
+    }
+    return true;
+}
+
+function checkRow(symbol, rowId) {
+    for (let columnIdx = 0;columnIdx < GRID_LENGTH; columnIdx++) {
+        if (grid[rowId][columnIdx] !== symbol) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkVictory(symbol){
+        let userRowVictory = false;
+        let userColumnVictory = false;
+        for(let colIdx=0;colIdx<GRID_LENGTH;colIdx++){
+            userColumnVictory = userColumnVictory || checkColumn(symbol,colIdx)
+            if(userColumnVictory) return true;
+        }
+        for(let rowIdx=0;rowIdx<GRID_LENGTH;rowIdx++){
+            userRowVictory = userRowVictory || checkRow(symbol,rowIdx)
+            if(userRowVictory) return true;
+        }
+        return userRowVictory||userColumnVictory || checkLeftDiagonal(symbol) || checkRightDiagonal(symbol);
+}
+
+function checkLeftDiagonal(symbol) {
+    let isWinner = false;
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        if (grid[rowIdx][rowIdx] === symbol) {
+            isWinner = true;
+        }
+        else
+            return false;
+    }
+    return isWinner;
+}
+
+function checkRightDiagonal(symbol) {
+    let isWinner = false;
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        if (grid[rowIdx][GRID_LENGTH - rowIdx - 1] === symbol) {
+            isWinner = true;
+        }
+        else
+            return false;
+    }
+    return isWinner;
+}
+
+function alertUserAndRestart(message){
+    alert(message);
+    start();
+}
+
+function checkWinner() {
+    let isUserWinner = checkVictory(1);
+    let isSystemWinner =  checkVictory(2);
+    let isGameOver = isUserWinner || isSystemWinner;
+    if(isGameOver){
+        isUserWinner ?  setTimeout(()=>alertUserAndRestart(USER_WINNING_MESSAGE),500) : setTimeout(()=>alertUserAndRestart(USER_LOSE_MESSAGE),500); 
+        return true;
+    }
+    return false;
+}
+
+function renderSystemMove() {
+    var randomRowId = Math.floor((Math.random() * 3));
+    var randomColumnId = Math.floor((Math.random() * 3));
+    if (grid[randomRowId][randomColumnId] === 0) {
+        grid[randomRowId][randomColumnId] = 2;
+        return;
+    }
+    else if (!isGridFull()) {
+        renderSystemMove();
+    }
+    else if(isGridFull()){
+        alert(GAME_OVER_MESSAGE);
+        start();
+    }
+}
+
+function addClickHandlers() {
+    var boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        boxes[idx].addEventListener('click', onBoxClick, false);
+    }
+}
+
+function start(){
+    initializeGrid();
+    renderMainGrid();
+    addClickHandlers(); 
+}
+
+start();
